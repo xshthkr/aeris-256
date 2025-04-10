9b5d2d80f369acc33715dcd94ac8396614f84a08
xshthkr <shashankt2004@gmail.com>
2025-04-10 06:11:19 +0530

    nothing major

--- src/utils.c
+++ src/utils.c
@@ -1,10 +1,9 @@
 #include <utils.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <stddef.h>
 
-void hex_to_bytes(const char* hex, uint8_t* out, size_t out_len) {
-    for (size_t i = 0; i < out_len; ++i) {
+#include <stdio.h>
+
+void hex_to_bin(const char* hex, uint8_t* out, size_t len) {
+    for (size_t i = 0; i < len; ++i) {
         sscanf(hex + 2*i, "%2hhx", &out[i]);
     }
 }
