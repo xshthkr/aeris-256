9f9337a1603578338bc31fca2512cebd20060d27
xshthkr <shashankt2004@gmail.com>
2025-04-10 06:10:03 +0530

    minor changes

--- include/key_schedule.h
+++ include/key_schedule.h
@@ -4,6 +4,6 @@
 #include <stdint.h>
 
 uint8_t* generate_master_key(uint8_t* password, uint8_t* salt);
-void generate_round_keys(uint8_t* key, uint64_t* round_keys, int rounds);
+void generate_round_keys(uint8_t* master_key, uint64_t* round_keys);
 
 #endif // KEY_SCHEDULE_H
